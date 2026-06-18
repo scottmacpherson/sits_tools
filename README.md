6c1694b253b43294d086f4e7b2fd5917556ac1ed
Scott Macpherson <scott@zerosleeps.com>
2026-06-18 10:30:11 +1000

    Send output from fix_uniface_list_of_json_objects.py to stdout

--- README.md
+++ README.md
@@ -32,4 +32,4 @@ Python script for building SSO links externally. Only works for AES-CBC.
 
 ### [fix_uniface_list_of_json_objects.py](fix_uniface_list_of_json_objects.py)
 
-Python script to turn a Uniface List of JSON objects into a valid JSON file.
+Python script to turn a Uniface List of JSON objects into a valid JSON list.
--- fix_uniface_list_of_json_objects.py
+++ fix_uniface_list_of_json_objects.py
@@ -1,4 +1,4 @@
-"""Turn a Uniface List of JSON objects into a valid JSON file
+"""Turn a Uniface List of JSON objects into a valid JSON list
 
 One use case is for dealing with the output of a `SELECT JSON_OBJECT()…` query
 via SWB. Set the output to Uniface List, save the result, and shove it through
@@ -7,16 +7,17 @@ this script.
 
 import argparse
 import json
+import pathlib
+import sys
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("input_file")
-    parser.add_argument("output_file")
+    parser.add_argument("input_file", type=pathlib.Path)
     args = parser.parse_args()
 
     with open(args.input_file, "r") as f:
         raw_input = f.read()
     fixed_input = "[" + raw_input.replace("\x1b", ",") + "]"
-    json_output = json.loads(fixed_input)
-    with open(args.output_file, "w") as f:
-        json.dump(json_output, f, indent=4)
+    json_object = json.loads(fixed_input)
+
+    json.dump(json_object, sys.stdout, indent=4)
